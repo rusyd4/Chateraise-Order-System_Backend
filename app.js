fc0a93509ff9d4e74fdc6a564770348168104a63
rusyd4 <naufalrs9708@gmail.com>
2025-05-30 07:45:35 +0700

    Add product photo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
@@ -8,9 +9,13 @@ const branchRoutes = require('./routes/branchRoutes');
 
 app.use(cors());
 app.use(express.json());
+
+// Serve uploads folder as static files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
 app.use('/branch', branchRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
